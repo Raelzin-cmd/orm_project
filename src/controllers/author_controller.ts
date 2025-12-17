c1da8e629ed9b30d2da209414611f303bfa039ed
Israel Almeida <almeidev.rael@gmail.com>
2025-12-16 21:38:32 -0300

    Implementação de listagem, atualização, pesquisa e exclusão de autor

--- src/controllers/author_controller.ts
+++ src/controllers/author_controller.ts
@@ -37,19 +37,126 @@ export default class AuthorController {
         }
     }
 
+
     async list(req: Request, res: Response) {
 
+        try {
+            const authors = await prisma.author.findMany()
+            return res.status(200).json(authors)
+        } catch (error) {
+            const erro = error as Error
+            return res.status(400).json({
+                message: erro.message
+            })
+        }
     }
+
 
     async show(req: Request, res: Response) {
+        const { id } = req.params
+        try {
+            const author = await prisma.author.findUnique({
+                where: {
+                    id: Number(id)
+                }
+            })
 
+            if (!author) {
+                return res.status(404).json({
+                    message: 'No authors found'
+                })
+            }
+
+            return res.status(200).json(author)
+        } catch (error) {
+            const erro = error as Error
+            return res.status(400).json({
+                message: erro.message
+            })
+        }
     }
+
 
     async update(req: Request, res: Response) {
+        const { id } = req.params
+        const { name, email, bio, cpf, country } = req.body
 
+        try {
+            const author = await prisma.author.findUnique({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            if (!author) {
+                return res.status(404).json({
+                    message: 'No authors found'
+                })
+            }
+
+            const emailExists = await prisma.author.findUnique({
+                where: {
+                    email
+                }
+            })
+
+            if (emailExists && emailExists.email !== email) {
+                return res.status(400).json({
+                    message: 'Email already exists'
+                })
+            }
+
+            await prisma.author.update({
+                where: {
+                    id: Number(id)
+                },
+                data: {
+                    name,
+                    email,
+                    bio,
+                    cpf,
+                    pais: country
+                }
+            })
+
+            return res.status(204).send()
+        } catch (error) {
+            const erro = error as Error
+            return res.status(400).json({
+                message: erro.message
+            })
+        }
     }
 
-    async delete(req: Request, res: Response) {
 
+    async delete(req: Request, res: Response) {
+        const { id } = req.params
+
+        try {
+            const author = await prisma.author.findUnique({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            if (!author) {
+                return res.status(404).json({
+                    message: 'No authors found'
+                })
+            }
+
+            await prisma.author.delete({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(204).send()
+        } catch (error) {
+            const erro = error as Error
+            return res.status(400).json({
+                message: erro.message
+            })
+        }
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,9 @@ import AuthorController from './controllers/author_controller'
 const routes = Router()
 
 routes.post('/authors', new AuthorController().create)
+routes.get('/authors', new AuthorController().list)
+routes.get('/authors/:id', new AuthorController().show)
+routes.put('/authors/:id', new AuthorController().update)
+routes.delete('/authors/:id', new AuthorController().delete)
 
 export default routes
